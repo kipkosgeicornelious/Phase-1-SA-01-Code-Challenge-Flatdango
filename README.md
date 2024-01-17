f19c1c04081730f83b7fee5a51ef58bf0fc25ff3
kipkosgeicornelious <kipkosgeicornelious6@gmail.com>
2024-01-17 06:28:28 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Phase-1-SA-01-Code-Challenge-Flatdango
+## <b>Author</b>
+
+Cornelious kipkosgei
+## Project Description
+
+This project implements a mini web application using JavaScript.
+
+Flatiron Movie Theater is open for business! You will be building out an
+application, Flatdango, that allows a user to purchase movie tickets from the
+theater.
+## Requirements
+
+*vs code
+
+*json-server
+
+*Fetch data from a local server running JSON DB server
+
+## Installations
+To install any dependancies use npm install
+
+## Getting started
+To run this application:
+
+*Clone from here
+[githubrepo]
+(git@github.com:kipkosgeicornelious/Phase-1-SA-01-Code-Challenge-Flatdango.git
+)
+
+*Run this command 
+`json-server --watch db.json`  
+# To view the published HTML as a website -
+To view this website, copy and paste this link onto your browser, make sure you are running the json server.
+
+
+ https://github.com/kipkosgeicornelious/Phase-1-SA-01-Code-Challenge-Flatdango.git
+## License
+MIT license
---
+++ index.js
@@ -0,0 +1,93 @@
+const filmTime = document.getElementById('film-item')
+//declare the json url as an API for easy access.
+const API = "http://localhost:3000/films";
+
+fetch(API)
+.then((res) => res.json())
+.then(renderFilm) 
+ //const allMovies = renderFilm;
+
+
+//rendering only the one poster and its details.
+function renderFilm(film) {
+    const filmMenuDiv = document.getElementById("film-menu");
+    const titleDiv = document.getElementById("film-details"); 
+    const runDiv = document.getElementById("runtime")
+    const showDiv = document.getElementById("showtime")
+    const availableDiv = document.getElementById("Available-tickets")
+
+    filmMenuDiv.src = film.poster;
+
+    const filmTitle = document.createElement("p");
+    filmTitle.textContent = film.title;
+    titleDiv.append(filmTitle);
+
+    const filmRuntime = document.createElement("p");
+    filmRuntime.textContent = film.runtime;
+    runDiv.append(filmRuntime);
+
+    const filmShowtime = document.createElement("p");
+    filmShowtime.textContent = film.showtime;
+    showDiv.append(filmShowtime);
+
+    const filmAvailability = document.createElement("p");
+    filmAvailability.textContent = (film.capacity - film.tickets_sold);
+    availableDiv.append(filmAvailability);
+     
+    //for the buying of tickets button 
+    const button = document.getElementById("sell");
+    const btn = parseInt(availableDiv.innerText);
+    console.log(btn);
+    button.addEventListener('click', () => {
+       if (btn >= 1) {
+        availableDiv.innerText = (availableDiv.innerText - 1)
+        
+       }
+      
+       if (availableDiv.innerText < 0) {
+        availableDiv.innerText = '0';
+        alert ('Sorry, we are currently sold out of tickets for this film.')
+       }
+
+       /*if (availableDiv.innerHTML <= 0) {
+        button.innerHTML = 'SOLD OUT'
+       }*/
+
+    
+    })
+
+}
+//a second fetch request to get the movie list.
+function getFilms(){
+    fetch("http://localhost:3000/films")
+    .then((response) => response.json())
+    .then(renderFilms);
+}
+getFilms();
+
+function renderFilms(films) {
+    films.forEach(filmDetails);
+}
+
+function filmDetails(details) {
+    const titlesElement = document.getElementById("film-item");
+
+    let listElement = document.createElement("li");
+    listElement.innerText = details.title;
+    listElement.className = 'film-item'
+    
+    let imgElement = document.createElement("film-details");
+    imgElement.src = details.poster;
+
+  const moviePoster = document.getElementById('film-menu')
+
+    titlesElement.append(listElement);
+    listElement.addEventListener('click', () => {
+        console.log(details.title)
+        document.getElementById('film-details').innerText = details.title
+        document.getElementById('runtime').innerText = details.runtime
+        document.getElementById('showtime').innerText = details.showtime
+        document.getElementById('Available-tickets').innerText = details.capacity - details.tickets_sold
+        moviePoster.src = details.poster
+    })
+}
